d85497f2c20401ec65eede4451d5dc3766832166
emerihcelestin <emerih02@gmail.com>
2019-08-12 11:37:27 +0300

    Modification et destroy

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -9,26 +9,48 @@ class CommentController < ApplicationController
   end
 
  def create
-    id = params['id'] 
-    content = params['content']
-    @comment = Comment.new(content: content,gossip_id: id, user_id: 11) # avec xxx qui sont les données obtenues à partir du formulaire
+    id = params[:gossip_id]
+    @gossip = Gossip.find(id)
+    
+    anonyme = User.find(11)
+
+    content = params[:content]
+
+    @comment = Comment.new(content: content)
+    @comment.user = anonyme
+    @comment.gossip = @gossip
+    @user = @gossip.user
+    @city = @user.city 
 
     if @comment.save # essaie de sauvegarder en base @gossip
       # si ça marche, il redirige vers la page d'index du site
-      redirect_to gossips_path, :notice => "The super potin was succesfully saved !"
+      redirect_to @gossip, :notice => "New comment!"
 
     else
       # sinon, il render la view new (qui est celle sur laquelle on est déjà)
-      render'new'
+      render 'gossips/show'
     end
-    
+  end 
 
   def edit
+    @comment = Comment.find(params['id'])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @gossip = @comment.gossip
+    content = params['content']
+    if @comment.update(content: content)
+      redirect_to @gossip, :alert => "Comment was succesfully updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @gossip = @comment.gossip
+    @comment.destroy
+    redirect_to @gossip, :notice => "Comment Removed"
   end
 end
